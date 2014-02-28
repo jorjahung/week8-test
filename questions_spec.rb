9a6dd58551eb68f7a4c0e11dcbde14d75d6d5531
Jorja Hung <jorjahung@gmail.com>
2014-02-28 17:13:04 +0000

    FizzBuzz added in

--- questions_spec.rb
+++ questions_spec.rb
@@ -216,5 +216,6 @@ describe 'the Friday test :)' do
     n = count_words_of_each_length_in_a_file('lorem.txt') || []
     expect(Hash[n.sort]).to eq({1=>1, 2=>5, 3=>7, 4=>12, 5=>14, 6=>4, 7=>8, 8=>6, 9=>6, 10=>2, 11=>2, 12=>3}) 
   end
-  
+
+
 end
