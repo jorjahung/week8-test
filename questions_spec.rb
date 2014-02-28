a2ff31579c2f39064b9d4eaa7eab5ca0f4b4c671
Jorja Hung <jorjahung@gmail.com>
2014-02-28 15:09:01 +0000

    33 down

--- questions_spec.rb
+++ questions_spec.rb
@@ -162,33 +162,33 @@ describe 'the Friday test :)' do
     expect(n).to eq 'The Lion the Witch and the Wardrobe'
   end
 
-  # specify 'check_a_string_for_special_characters' do
-  #   a = check_a_string_for_special_characters 'ABCdef123'
-  #   b = check_a_string_for_special_characters 'ABC@def123!'
+  specify 'check_a_string_for_special_characters' do
+    a = check_a_string_for_special_characters 'ABCdef123'
+    b = check_a_string_for_special_characters 'ABC@def123!'
 
-  #   expect(a).to be_false
-  #   expect(b).to be_true
-  # end
+    expect(a).to be_false
+    expect(b).to be_true
+  end
 
-  # specify 'get_upper_limit_of' do
-  #   n = get_upper_limit_of 1..20
-  #   expect(n).to eq 20
-  # end
+  specify 'get_upper_limit_of' do
+    n = get_upper_limit_of 1..20
+    expect(n).to eq 20
+  end
 
-  # specify 'is_a_3_dot_range?' do
-  #   a = is_a_3_dot_range? 1..20
-  #   b = is_a_3_dot_range? 1...20
+  specify 'is_a_3_dot_range?' do
+    a = is_a_3_dot_range? 1..20
+    b = is_a_3_dot_range? 1...20
 
-  #   expect(a).to be_false
-  #   expect(b).to be_true
-  # end
+    expect(a).to be_false
+    expect(b).to be_true
+  end
 
-  # specify 'square_root_of' do
-  #   a = square_root_of 9
-  #   b = square_root_of 3
-  #   expect(a).to eq 3.0
-  #   expect(b).to eq 1.7320508075688772
-  # end
+  specify 'square_root_of' do
+    a = square_root_of 9
+    b = square_root_of 3
+    expect(a).to eq 3.0
+    expect(b).to eq 1.7320508075688772
+  end
 
   # specify 'word_count_a_file' do
   #   n = word_count_a_file 'lorem.txt'
